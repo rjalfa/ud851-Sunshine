aa7775e46ab5e280e4a9f106a86aaee60a117807
Rounaq Jhunjhunu Wala <rounaqwl66@gmail.com>
2018-05-11 20:46:20 +0530

    S04.02

--- S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -11,7 +15,13 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        String text = "No weather for you!";
+        Intent launcherIntent = getIntent();
 
-        // TODO (2) Display the weather forecast that was passed from MainActivity
+        if(launcherIntent.hasExtra(Intent.EXTRA_TEXT)) {
+           text = launcherIntent.getStringExtra(Intent.EXTRA_TEXT);
+        }
+        // COMPLETE (2) Display the weather forecast that was passed from MainActivity
+        ((TextView) findViewById(R.id.detail_activity_text)).setText(text);
     }
 }
